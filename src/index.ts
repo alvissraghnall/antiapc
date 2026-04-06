42fe1158d85dd2b31379ba35c1ed7f31f026be65
Alviss Raghnall <isiomastewart@gmail.com>
2026-04-06 19:08:30 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { Hono } from 'hono'
+import { serveStatic } from 'hono/cloudflare-workers'
+import { logger } from 'hono/logger'
+
+const app = new Hono()
+
+app.use(logger())
+
+app.get('/star', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/', async (c) => {
+  return serveStatic({
+    root: './public',
+    manifest: {
+      'index.html': 'index.html',
+    },
+  })
+})
+
+app.post('/', async (c) => {
+  const body = await c.req.parseBody()
+
+  return c.text('OK')
+})
+
+export default app
